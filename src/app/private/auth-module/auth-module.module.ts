7fb46ab403275db17073c0ecfcaf4c83e2565274
João Paulo Polles <joaopaulopolles@gmail.com>
2023-06-27 13:12:33 -0300

    início do desenvolvimento do módulo de autenticação

---
+++ src/app/private/auth-module/auth-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthModuleComponent } from './auth-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthModuleComponent]
+})
+export class AuthModuleModule { }
